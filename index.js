3dc2636b2121b3bd877a5be23c6895f5e9e19833
Philip <philip119@gmx.de>
2020-04-30 21:05:53 +0200

    Okee

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 const min = 5000;
 const max = 7000;
 
+const onlyLogExtraPoints = false;
+
 yeet();
 
 async function yeet(){
@@ -10,7 +12,8 @@ async function yeet(){
         console.log("[" + new Date().toISOString() + "] YEEETED EXTRA POINTS!!! MOFOGGU");
         document.querySelector(".community-points-summary button.tw-button").click()
     }else{
-        console.log("[" + new Date().toISOString() + "] No extra points :(");
+        if(!onlyLogExtraPoints)
+            console.log("[" + new Date().toISOString() + "] No extra points :(");
     }
         
     await sleep(min + Math.floor(Math.random() * (max - min)));
