5c569a7b2a2648cce9f26df0dfd83775e11f9ea2
Philip <philip119@gmx.de>
2020-04-30 21:03:20 +0200

    Yeet

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Simptch
  Simping script for gaining dem points
+
+
+- Open a Twitch stream
+- Open the console
+- Paste the whole content from index.js into the console
+- Profit!
+
+## Advanced Users
+
+`min` is the minimum waiting time in ms
+`max` is the maximum waiting time in ms
+
+so feel free to change the variables
+
+Happy simping
+
---
+++ index.js
@@ -0,0 +1,19 @@
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const min = 5000;
+const max = 7000;
+
+yeet();
+
+async function yeet(){
+    if(document.querySelector(".community-points-summary button.tw-button")){
+        console.log("["+new Date().toISOString()+"] YEEETED EXTRA POINTS!!! MOFOGGU");
+        document.querySelector(".community-points-summary button.tw-button").click()
+    }else{
+        console.log("["+new Date().toISOString()+"] No extra points :(");
+    }
+        
+    await sleep(min + Math.floor(Math.random() * (max - min)));
+
+    return yeet();
+}
