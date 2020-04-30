6833245a0bb7ae600efd77cd88bb502e14f03193
Philip <philip119@gmx.de>
2020-04-30 21:04:50 +0200

    k

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ yeet();
 
 async function yeet(){
     if(document.querySelector(".community-points-summary button.tw-button")){
-        console.log("["+new Date().toISOString()+"] YEEETED EXTRA POINTS!!! MOFOGGU");
+        console.log("[" + new Date().toISOString() + "] YEEETED EXTRA POINTS!!! MOFOGGU");
         document.querySelector(".community-points-summary button.tw-button").click()
     }else{
-        console.log("["+new Date().toISOString()+"] No extra points :(");
+        console.log("[" + new Date().toISOString() + "] No extra points :(");
     }
         
     await sleep(min + Math.floor(Math.random() * (max - min)));
